e6e466ab433dcf9b90a931ee324caf3f663baa0d
TylerCEdge <tylercedge@gmail.com>
2019-06-06 15:04:12 -0400

    followed tutorial and completed createtodo component

--- src/App.js
+++ src/App.js
@@ -6,14 +6,47 @@ import CreateTodo from "./components/create-todo.component";
 import EditTodo from "./components/edit-todo.component";
 import TodosList from "./components/todos-list.component";
 
+import logo from "./logo.png";
+
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="container">
-          <h2>MERN-Stack Game App</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a
+              className="navbar-brand"
+              href="https://codingthesmartway.com"
+              target="_blank"
+            >
+              <img src={logo} width="30" height="30" alt="Your A Wizard" />
+            </a>
+            <Link to="/" className="navbar-brand">
+              MERN-Stack Todo App
+            </Link>
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">
+                    Todos
+                  </Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">
+                    Create Todos
+                  </Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/edit/:id" className="nav-link">
+                    Edit Todos
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
           <Route path="/" exact component={TodosList} />
-          <Route path="edit/:id" exact component={EditTodo} />
+          <Route path="/edit/:id" exact component={EditTodo} />
           <Route path="/create" exact component={CreateTodo} />
         </div>
       </Router>
