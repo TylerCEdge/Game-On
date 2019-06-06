bf5e30a371eb609ff3a764a8a30abc2eb8f4b647
atkmstr <atkmstr@localhost.localdomain>
2019-06-06 03:35:10 -0400

    app.js modified with router dependencies added and a dummy route added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-dom-router";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <h2>MERN-Stack Game App</h2>
+        </div>
+        <Route path="/" exact component={nameHere} />
+      </Router>
+    );
+  }
 }
 
 export default App;
