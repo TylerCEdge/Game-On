85207e5068a0d51b7a500e4d52d933c21c523a6c
atkmstr <atkmstr@localhost.localdomain>
2019-06-06 21:30:54 -0400

    edited console.log in backend server.js file to say listening on port + PORT

--- backend/server.js
+++ backend/server.js
@@ -68,5 +68,5 @@ todoRoutes.route('/update/:id').post(function(req, res) {
 app.use('/todos', todoRoutes);
 
 app.listen(PORT, function() {
-    console.log("Server is running on http://localhost: " + PORT);
+    console.log("Server is running on PORT " + PORT);
 });
