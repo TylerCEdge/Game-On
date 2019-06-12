f7954dec69fba3cca1ea3566dd75c22fb3f7192d
woodtop <cwood89@mac.com>
2019-06-12 12:35:58 -0400

    image slider working, just need optimized images

--- backend/API/index.js
+++ backend/API/index.js
@@ -13,14 +13,36 @@ module.exports = function (app) { //passing in express from server.js as app
         'Accept': 'application/json',
         'user-key': process.env.IGDB_KEY
       },
-      data: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,collection,cover,created_at,dlcs,expansions,external_games,first_release_date,follows,franchise,franchises,game_engines,game_modes,genres,hypes,involved_companies,keywords,multiplayer_modes,name,parent_game,platforms,player_perspectives,popularity,pulse_count,rating,rating_count,release_dates,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,time_to_beat,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;"
+      data: "fields name,summary,popularity; sort popularity desc; exclude tags;"
     })
       .then(response => {
         res.send(response.data)
-        console.log(response.data);
       })
       .catch(err => {
         console.error(err);
       });
   });
+
+  // Gets random game artwork image url
+  app.get("/api/images", (req, res) => {
+    axios({
+      url: "https://api-v3.igdb.com/covers",
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'user-key': process.env.IGDB_KEY
+      },
+      data: "fields alpha_channel,animated,game,height,image_id,url,width;"
+    })
+      .then(response => {
+        let urls = response.data.map(key => {
+          return key.url
+        })
+        res.send(urls)
+        console.log(urls);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  })
 }
