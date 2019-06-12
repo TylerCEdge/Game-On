b8e21632cc985ae1b5edc6bcc1de99734855c0ac
woodtop <cwood89@mac.com>
2019-06-12 12:44:20 -0400

    merging

--- frontend/src/components/todos-list/todos-list.component.js
+++ frontend/src/components/todos-list/todos-list.component.js
@@ -25,17 +25,17 @@ const Todo = props => (
 
 export default class TodosList extends Component {
 
-    constructor (props) {
+    constructor(props) {
         super(props);
-        this.state = {todos: []};
+        this.state = { todos: [] };
     }
 
     componentDidMount() {
         axios.get('http://localhost:4000/todos/')
             .then(response => {
-                this.setState({todos: response.data});
+                this.setState({ todos: response.data });
             })
-            .catch(function(error) {
+            .catch(function (error) {
                 console.log(error);
             })
     }
@@ -43,15 +43,15 @@ export default class TodosList extends Component {
     componentDidUpdate() {
         axios.get('http://localhost:4000/todos/')
             .then(response => {
-                this.setState({todos: response.data});
+                this.setState({ todos: response.data });
             })
-            .catch(function(error) {
+            .catch(function (error) {
                 console.log(error);
             })
     }
 
     todoList() {
-        return this.state.todos.map(function(currentTodo, i) {
+        return this.state.todos.map(function (currentTodo, i) {
             return <Todo todo={currentTodo} key={i} />;
         })
     }
@@ -59,33 +59,32 @@ export default class TodosList extends Component {
     render() {
         return (
             <div>
-                <h3>Coming Soon</h3>
 
                 <div className="carousel">
-                <Carousel />
-                </div>
-                
-                <div id="container-wrapper">
-                <div id="container">
-                
+                    <Carousel />
                 </div>
 
-                <table className="table table-striped" style={{ marginTop: 30 }}>
-                    <thead>
-                        <tr>
-                            <th>Game Name</th>
-                            <th>Game Type</th>
-                            <th>Favoritism</th>
-                            <th>Actions</th>
-                            {/* <th><DeleteBtn/></th> */}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        { this.todoList() }
-                    </tbody>
-                </table>
+                <div id="container-wrapper">
+                    <div id="container">
+
+                    </div>
+
+                    <table className="table table-striped" style={{ marginTop: 30 }}>
+                        <thead>
+                            <tr>
+                                <th>Game Name</th>
+                                <th>Game Type</th>
+                                <th>Favoritism</th>
+                                <th>Actions</th>
+                                {/* <th><DeleteBtn/></th> */}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.todoList()}
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            </div>  
         )
     }
 }
