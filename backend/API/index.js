66f5812dd4ec79885a151fc78bc31745d06809f0
woodtop <cwood89@mac.com>
2019-06-18 14:53:53 -0400

    reworked query for search functionality

--- backend/API/index.js
+++ backend/API/index.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 module.exports = function (app) { //passing in express from server.js as app
 
   //Search for Popular Games
-  app.get("/api/popular", (req, res) => {
+  app.get("/api/games/:game", (req, res) => {
     // getting all games
     let results = [];
     axios({
@@ -15,9 +15,10 @@ module.exports = function (app) { //passing in express from server.js as app
         'Accept': 'application/json',
         'user-key': process.env.IGDB_KEY
       },
-      data: "fields *; sort popularity desc;"
+      data: `fields *; search "${req.params.game}"; limit 20;`
     })
       .then(response => {
+        console.log(response.data)
         res.send(response.data)
       })
       .catch(err => {
@@ -28,7 +29,6 @@ module.exports = function (app) { //passing in express from server.js as app
 
   // Gets game artwork image url
   app.get("/api/images/:id", (req, res) => {
-    console.log(req.params.id)
     axios({
       url: "https://api-v3.igdb.com/covers",
       method: 'POST',
@@ -39,12 +39,10 @@ module.exports = function (app) { //passing in express from server.js as app
       data: `fields alpha_channel,animated,game,height,image_id,url,width; where id = ${req.params.id};`
     })
       .then(response => {
-        console.log(`======image==============`)
-        console.log(response.data[0].image_id)
         res.send(`https://images.igdb.com/igdb/image/upload/t_logo_med_2x/${response.data[0].image_id}.png`)
       })
       .catch(err => {
-        console.error(err);
+        // console.error(err);
       });
   })
 }
